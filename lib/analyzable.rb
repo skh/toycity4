13765e4453c630b6e3f8b556491ca417574a0768
Sonja Krause-Harder <skh@rcane.de>
2016-04-25 19:28:11 +0200

    Implement Analyzable::average_price and Analyzable::count_by_brand

--- lib/analyzable.rb
+++ lib/analyzable.rb
@@ -1,3 +1,21 @@
 module Analyzable
-  # Your code goes here!
+  def average_price(items)
+  	sum = 0
+  	items.each do |item|
+  		sum += item.price.to_f
+  	end
+  	avg = (sum / items.length).round(2)
+  end
+
+  def count_by_brand(items)
+  	result = {}
+  	items.each do |item|
+  		if result.has_key?(item.brand)
+  			result[item.brand] += 1
+  		else
+  			result[item.brand] = 1
+  		end
+  	end
+  	return result
+  end
 end
